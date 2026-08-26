3e750c64f6a4798fd66ce9230e43d2730639964e
Chenpi-mc <Chenpi_mc@163.com>
2026-08-26 15:44:10 +0800

    回滚 fish/kitty/niri blur/waybar 字体到 90efc7a 之前

--- dotfiles/.config/fish/functions/seeclip.fish
+++
@@ -1,7 +0,0 @@
-function seeclip --description='在终端里显示剪贴板中的图片'
-    wl-paste -t image/png 2>/dev/null | kitty +kitten icat
-    if test $status -ne 0
-        echo "剪贴板里没有图片"
-        return 1
-    end
-end
